27910d1ce562a36980dcaa7044ed68493c25ff46
wanqingying <yangjing>
2018-09-21 16:50:23 +0800

    dataResource/4

--- src/components/DataResource/ListContent.js
+++ src/components/DataResource/ListContent.js
@@ -5,7 +5,7 @@ class ListContent extends Component {
   render() {
     return (
       <div style={{padding: 24, background: '#fff', minHeight: 500}}>
-        <div className={styles["content-flex-box"]}>
+        <div className={styles['content-flex-box']}>
           <div>电子底账发票资源库</div>
           <div>互联网采集资源库</div>
           <div>大屏展示资源库</div>
--- src/routes/dataResource/resourceManage.js
+++ src/routes/dataResource/resourceManage.js
@@ -1,8 +1,41 @@
 import React, {Component} from 'react';
-import ManageTable from "../../components/DataResource/ManageTable"
+import ManageTable from '../../components/DataResource/ManageTable'
 import styles from './resourceManage.less';
-import {connect} from "dva";
+import {connect} from 'dva';
+import {Divider} from 'antd'
 
+
+function getColumn(data) {
+  const columns = [{
+    title: 'Name',
+    dataIndex: 'name',
+    key: 'name'
+  }, {
+    title: 'Age',
+    dataIndex: 'type',
+    key: 'type'
+  }, {
+    title: 'Address',
+    dataIndex: 'org',
+    key: 'org'
+  }, {
+    title: 'detail',
+    dataIndex: 'comment',
+    key: 'comment'
+  }, {
+    title: 'Action',
+    key: 'action',
+    render: (text, record) => (
+      <span>
+      <a href="javascript:;">Invite {record.name}</a>
+      <Divider type="vertical"/>
+      <a href="javascript:;">Delete</a>
+    </span>
+    )
+  }];
+  console.log(columns)
+  return columns;
+}
 
 class ResourceManage extends Component {
   constructor(props) {
@@ -10,30 +43,38 @@ class ResourceManage extends Component {
     this.doUpdate = this.doUpdate.bind(this);
     this.state = {
       tableList: null,
+      columns: null
     }
   }
 
 
   componentDidMount() {
     this.props.dispatch({
-      type: "resource/getTableList",
+      type: 'resource/getTableList',
       payload: {},
       callback: (res) => {
-        this.doUpdate("tableList", res.data.datalist);
-      },
+        this.doUpdate('tableList', res.data.datalist)
+      }
     })
   }
 
   doUpdate(state, data) {
-    this.setState({[state]: data});
+    if (state === 'tableList') {
+      let col = getColumn(data);
+      this.setState({tableList: data, columns: col});
+    }
   }
+
   render() {
     return (
-      <div className={styles["manage-table"]}>
-        <div className={"table-header"}>
+      <div className={styles['manage-table']}>
+        <div className={'table-header'}>
           <span>当前查看: 资源目录列表</span>
         </div>
-        <ManageTable {...this.props} {...this.state} />
+        <ManageTable
+          {...this.props}
+          {...this.state}
+        />
       </div>
     );
   }
