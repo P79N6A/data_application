4325d8571de12e13e21d6082e03358a009c290ac
tangquankun <1072317596@qq.com>
2018-09-30 15:10:39 +0800

    search and table

--- src/pages/dataResource/Approval/ApprovalTable/index.js
+++ src/pages/dataResource/Approval/ApprovalTable/index.js
@@ -1,31 +1,36 @@
-import React, { Component } from 'react'
-import { Table } from 'antd'
-class ApprovalTable extends Component{
+import React, { Component } from 'react';
+import { Table } from 'antd';
+class ApprovalTable extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      columns:[
+      columns: [
         {
           title: '接口名',
           dataIndex: 'name',
-          key: 'name'
-        }, {
+          key: 'name',
+        },
+        {
           title: '资源目录',
           dataIndex: 'catalog',
-          key: 'catalog'
-        }, {
+          key: 'catalog',
+        },
+        {
           title: '所属服务',
           dataIndex: 'services',
-          key: 'services'
-        }, {
+          key: 'services',
+        },
+        {
           title: '申请人',
           dataIndex: 'username',
-          key: 'username'
-        }, {
+          key: 'username',
+        },
+        {
           title: '申请时间',
           dataIndex: 'time',
-          key: 'time'
-        }, {
+          key: 'time',
+        },
+        {
           title: '操作',
           key: 'action',
           render: (text, record) => (
@@ -33,16 +38,14 @@ class ApprovalTable extends Component{
               <a href="#">同意</a>
               <a href="#">拒绝</a>
             </span>
-          )
-        }
+          ),
+        },
       ],
-      dataSource: []
-    }
+      dataSource: [],
+    };
   }
   render() {
-    return(
-      <Table  columns={this.state.columns} dataSource={this.state.dataSource}/>
-    )
+    return <Table columns={this.state.columns} dataSource={this.state.dataSource} />;
   }
 }
-export default ApprovalTable
+export default ApprovalTable;
--- src/pages/dataResource/Approval/index.js
+++ src/pages/dataResource/Approval/index.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import ApprovalSearch from './ApprovalSearch'
-import  ApprovalTable from './ApprovalTable'
+import ApprovalSearch from './ApprovalSearch';
+import ApprovalTable from './ApprovalTable';
 class Approval extends Component {
   constructor(props) {
     super(props);
   }
   render() {
     return (
-    <div>
-      <ApprovalSearch />
-      <ApprovalTable />
-    </div>
-    )
+      <div>
+        <ApprovalSearch />
+        <ApprovalTable />
+      </div>
+    );
   }
 }
 export default Approval;
