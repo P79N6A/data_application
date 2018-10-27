7f24585939a97ae648fc24adbc83a270539fc8e0
wanqingying <yangjing>
2018-10-27 09:18:57 +0800

    菜单可收缩

--- src/components/Global/GlobalHeader/index.js
+++ src/components/Global/GlobalHeader/index.js
@@ -27,7 +27,7 @@ export default class GlobalHeader extends PureComponent {
   };
 
   render() {
-    const { isMobile, logo } = this.props;
+    const {collapsed, isMobile, logo } = this.props;
     return (
       <div className={styles.header}>
         {isMobile && (
@@ -49,7 +49,14 @@ export default class GlobalHeader extends PureComponent {
             />
             <h1>精准社会服务平台</h1>
           </Link>
+          <Icon
+            className={styles.trigger}
+            style={{position:'absolute'}}
+            type={collapsed ? 'menu-unfold' : 'menu-fold'}
+            onClick={this.toggle}
+          />
         </div>
+
         <HeaderMenu {...this.props} />
         <RightContent {...this.props} />
       </div>
