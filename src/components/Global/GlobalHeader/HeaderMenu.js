cd48e546b3f51d4b9911ff9cca5f3576c8277e16
wanqingying <yangjing>
2018-10-27 18:26:35 +0800

    完成布局

--- src/components/Global/GlobalHeader/HeaderMenu.js
+++ src/components/Global/GlobalHeader/HeaderMenu.js
@@ -4,6 +4,7 @@ import Link from 'umi/link';
 import styles from './HeaderMenu.less';
 import {store} from '@/utils/utils';
 import { reloadAuthorized } from '@/utils/Authorized';
+import PropTypes from 'prop-types';
 
 class HeaderMenu extends Component {
   constructor(props) {
@@ -14,25 +15,31 @@ class HeaderMenu extends Component {
 
   // 切换路由菜单
     changePathAuth (path) {
-      this.setState({activePath:path});
+      this.props.dispatch({
+        type:'global/changeCurrentApp',
+        payload:{currentApp:path}
+      })
   }
 
   render() {
-    let activePath=this.state.activePath;
-
+    const {currentApp}=this.props;
     return (
       <span className={styles['header-menu']}>
         <Link onClick={()=>this.changePathAuth('data')}
-            className={!activePath || activePath==='data' ?styles['active-link']:''}
+            className={!currentApp || currentApp==='data' ?styles['active-link']:''}
             to={'/api'}
         > 数据平台</Link>
         <Link onClick={()=>this.changePathAuth('community')}
-            className={activePath==='community'?styles['active-link']:''}
+            className={currentApp==='community'?styles['active-link']:''}
             to={'/community'}
         >社区平台</Link>
       </span>
     );
   }
 }
+
+HeaderMenu.propTypes = {
+  currentApp: PropTypes.string
+};
 
 export default HeaderMenu;
