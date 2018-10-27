4399c1f8b57819776715a1e9a8567163c7702921
tangquankun <1072317596@qq.com>
2018-10-27 18:12:00 +0800

    wanc

---
+++ src/pages/community/map/panels/people.js
@@ -0,0 +1,24 @@
+import { Card, Col, Row } from 'antd';
+import React, { Component } from 'react';
+
+class People extends Component {
+  render() {
+    return (
+      <div style={{ background: '#ECECEC', padding: '30px' }}>
+        <Row gutter={16}>
+          <Col span={8}>
+            <Card title="常住人口" bordered={true}>5600</Card>
+          </Col>
+          <Col span={8}>
+            <Card title="暂住人口" bordered={true}>2500</Card>
+          </Col>
+          <Col span={8}>
+            <Card title="流动人口" bordered={true}>3600</Card>
+          </Col>
+        </Row>
+      </div>
+    );
+  }
+}
+
+export default People;
