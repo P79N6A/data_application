59ae453e692d037746205a5479111171f4ce1815
zhoukaiheng <KF.zhoukaiheng@h3c.com>
2018-10-31 18:44:48 +0800

    add:添加任务调度模块

--- src/components/Task/Header/index.js
+++ src/components/Task/Header/index.js
@@ -1,14 +1,32 @@
 import React, { PureComponent } from 'react';
-import { Breadcrumb } from 'antd';
+import { Breadcrumb, Button, Col, Input, Row} from 'antd';
 
 class Header extends PureComponent {
 
   render() {
     const Item = Breadcrumb.Item;
     return (
-      <Breadcrumb>
-        <Item>{this.props.title || ""}</Item>
-      </Breadcrumb>
+        <Row gutter={24} style={{marginBottom:24,marginTop:10}}>
+          <Col span={14}>
+          <Breadcrumb >
+            <Item>
+              <strong>
+                {this.props.title || ""}
+              </strong>
+            </Item>
+          </Breadcrumb>
+          </Col>
+          <Col span={6}>
+            <Input.Search
+              placeholder="input search text"
+              onSearch={value => console.log(value)}
+              enterButton
+            />
+          </Col>
+          <Col span={4}>
+            <Button type="success">+添加</Button>
+          </Col>
+        </Row>
     );
   }
 }
--- src/components/Task/Project/index.js
+++ src/components/Task/Project/index.js
@@ -1,15 +1,86 @@
 import React, {PureComponent} from 'react';
+import { Collapse, Tabs } from 'antd';
 
 import Header from '../Header';
+import { Tab } from '../../Login';
+
+const TabPane = Tabs.TabPane;
+const Panel =Collapse.Panel;
+
+
 
 class Project extends PureComponent {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        {
+          type: "person",
+          title: "任务1",
+          value: "上刀山下火海"
+        },
+        {
+          type: "person",
+          title: "任务2",
+          value: "吃饺子"
+        },
+        {
+          type: "person",
+          title: "任务3",
+          value: "玩勺子"
+        }
+      ]
+    }
+  }
+
+  _handleCallback = (key) => {
+    console.log(key);
+  };
+
+  _renderContent = (data) => {
+    const customStyle = {
+      width:500,
+    };
+
+    const customPanelStyle = {
+      background: '#f7f7f7',
+      marginBottom: 24,
+      overflow: 'hidden',
+    };
+
+    return (
+      <Collapse  bordered={false} style={customStyle} >
+        {data.map((item, index) => {
+          return(
+            <Panel header={item.title} key={index} style={customPanelStyle} >
+              {item.value}
+            </Panel>
+          )
+        })}
+      </Collapse>
+    )
+  };
 
   render() {
     return (
       <>
         <Header title="项目管理" />
         <div>
-          项目管理
+          <Tabs defaultActiveKey="person" onChange={this._handleCallback}>
+            <TabPane tab="个人" key="person" >
+              {this._renderContent(this.state.data)}
+            </TabPane>
+            <TabPane tab="工作组" key="group" >
+              工作组
+            </TabPane>
+            <TabPane tab="所有" key="all" >
+              所有
+            </TabPane>
+          </Tabs>
+          <div>
+
+          </div>
         </div>
       </>
     )
