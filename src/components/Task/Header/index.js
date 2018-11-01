e5ebd31be65c8c2a1881c746a6869c11cdad823a
zhoukaiheng <KF.zhoukaiheng@h3c.com>
2018-11-01 11:42:37 +0800

    Edit:修改页面样式为红色

--- src/components/Task/Header/index.js
+++ src/components/Task/Header/index.js
@@ -20,7 +20,7 @@ class Header extends PureComponent {
         )}
         {Add && (
           <Col span={4}>
-            <Button type="success">+添加</Button>
+            <Button type="primary">添加</Button>
           </Col>
         )}
         {Adv && (
