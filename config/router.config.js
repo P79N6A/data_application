340a7c0318495e7846e6b6edae04b46a939fc048
caijie <1004519755@qq.com>
2018-11-05 10:22:32 +0800

    菜单栏重复部分删除

--- config/router.config.js
+++ config/router.config.js
@@ -30,91 +30,6 @@ export default [
       { path: '/community', redirect: '/community/map' },
       { path: '/task', redirect: '/task/project' },
       /*= ================接口服务=============== */
-      {
-        path: '/api/monitor',
-        name: 'api.monitor',
-        hideInMenu: true,
-        component: './apiService/Monitor',
-      },
-      {
-        path: '/api/monitorApi',
-        name: 'api.monitorApi',
-        component: './apiService/Monitor/MonitorApi',
-      },
-      // {
-      //   path: '/api/apply',
-      //   name: 'monitorApi',
-      //   hideInMenu: true,
-      //   component: './apiService/Apply',
-      // },
-      {
-        path: '/api/manage/publish',
-        name: 'api.publish',
-        component: './apiService/Publish/AdvancedForm.js',
-      },
-      {
-        path: '/api/manage',
-        name: 'api.manage',
-        component: './apiService/ListManage',
-      },
-      {
-        path: '/api/approval',
-        name: 'api.approval',
-        component: './apiService/approval',
-      },
-      {
-        path: '/api/apply',
-        name: 'api.apply',
-        component: './apiService/Apply',
-      },
-      {
-        path: '/api/echart',
-        hideInMenu: true,
-        name: 'echart',
-        component: './testComponent',
-      },
-      /*= ================数据资源=============== */
-      {
-        path: '/data/mission',
-        name: 'data.mission',
-        component: './dataResource/CollectMission',
-      },
-      {
-        path: '/data/main',
-        name: 'data.main',
-        component: './dataResource/CollectMain',
-      },
-      {
-        path: '/data/file',
-        name: 'data.file',
-        component: './dataResource/CollectFile',
-      },
-      {
-        path: '/data/list',
-        name: 'data.list',
-        component: './dataResource/CollectList',
-      },
-      {
-        path: '/data/manage',
-        name: 'data.manage',
-        component: './dataResource/Manage',
-      },
-      /*= ================数据应用=============== */
-      {
-        path: '/application/main',
-        name: 'application.main',
-        component: './dataApplication/PyMain',
-      },
-      {
-        path: '/application/result',
-        name: 'application.result',
-        component: './dataApplication/PyResult',
-      },
-      {
-        path: '/application/analyze',
-        name: 'application.analyze',
-        component: './dataApplication/PyAnalyze',
-      },
           {
             path: '/api/monitor',
             name: 'api.monitor',
@@ -220,46 +135,6 @@ export default [
         name: 'process.clean',
         component: './DataProcess/Clean',
       },
-
-      // 资源目录
-      {
-        // 资源目录
-        path: '/catalog/catalog',
-        name: 'catalog.catalog',
-        component: './dataCatalog/Catalog',
-      },
-      {
-        // 资源管理
-        path: '/catalog/datamanage',
-        name: 'catalog.datamanage',
-        component: './dataCatalog/DataManage',
-      },
-      // 运维统计
-      {
-        // 数据监控
-        name: 'audit.dmonitor',
-        path: '/audit/dmonitor',
-        component: './dataAudit/DataMonitor',
-      },
-      {
-        // 系统监控
-        name: 'audit.ymonitor',
-        path: '/audit/ymonitor',
-        component: './dataAudit/SysMonitor',
-      },
-      {
-        // 数据统计
-        name: 'audit.statis',
-        path: '/audit/statis',
-        component: './dataAudit/Statis',
-      },
-      {
-        // 审计管理
-        name: 'audit.amanage',
-        path: '/audit/amanage',
-        component: './dataAudit/AuditManage',
-      },
-
           {
             // 资源目录管理
             path: '/catalog/catalog',
