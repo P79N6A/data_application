32415267ae4b65501dbf997171419d9c2f12a7b3
caijie <1004519755@qq.com>
2018-11-06 11:02:36 +0800

    ExecuteFlow Key添加

--- src/components/Task/Project/ExecuteFlow/AgreeSetting/index.js
+++ src/components/Task/Project/ExecuteFlow/AgreeSetting/index.js
@@ -7,11 +7,13 @@ const RadioGroup = Radio.Group;
 const { Option } = Select;
 const RedioList = [
     {
+      key:'1',
       value: '1',
       title: '跳过执行',
       cont: '不启动已经运行中的流程.',
   
     }, {
+      key:'2',
       value: '2',
       title: '一致执行',
       cont: '强制执行流程. 之前的执行不会被影响.',
@@ -26,22 +28,22 @@ class AgreeSetting extends Component {
                 <p className={styles.contTitle}>一致性执行选项</p>
                 <p>流程运行中有以下选项可以设置.</p>
                 <Divider />
-                <RadioGroup>
+                <RadioGroup defaultValue = "1">
                     {
                         RedioList.map((item) => {
                             return (
                                 <div>
-                                    <Radio value={item.value}>{item.title}</Radio>
+                                    <Radio value={item.value} key = {item.key}>{item.title}</Radio>
                                     <p>{item.cont}</p>
                                 </div>
                             );
                         })
 
                     }
-                    <Radio value='3' style={{ display: 'block' }}>排序执行</Radio>
+                    <Radio value='3' key = "3" style={{ display: 'block' }}>排序执行</Radio>
                     <Select defaultValue="1" style={{ width: 180, marginTop: 15 }}>
-                        <Option value='1'>level1</Option>
-                        <Option value='2'>level2</Option>
+                        <Option value='1' key="1">level1</Option>
+                        <Option value='2' key="2">level2</Option>
                     </Select>
                     <p>管道执行流程，使当前执行流程不会被覆盖.</p>
                     <ul className={styles.listStyleSet}>
